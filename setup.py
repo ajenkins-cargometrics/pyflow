0140d9f5dd49a2aef61ce1c1c63e9eaef8352e4c
ajenkins-cargometrics <ajenkins@cargometrics.com>
2017-04-06 15:38:41 -0400

    Changed package name to pyflow-swf

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='pyflow',
+    name='pyflow-swf',
     version='2.0',
     author='Cargometrics',
     author_email='ajenkins@cargometrics.com',
     packages=['pyflow'],
-    url='http://www.cargometrics.com',
-    description='Python Workflow Library',
+    url='https://github.com/ajenkins-cargometrics/pyflow',
+    description='Python Workflow Library built on the AWS SWF service',
     install_requires=[
         'boto3',
         'attrs'
     ],
     setup_requires=['pytest-runner'],
     tests_require=[
-        'pytest',
+        'pytest>=3.0',
     ]
 )
