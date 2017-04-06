9c1ad04d406cd364fd2e3956e6be4dbfb1765b67
ajenkins-cargometrics <ajenkins@cargometrics.com>
2017-04-06 14:33:14 -0400

    Travis CI integration, and PyPi configuration

--- setup.py
+++ setup.py
@@ -1,19 +1,35 @@
+import os
+
 from setuptools import setup
 
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
+
 setup(
-    name='pyflow',
-    version='2.0',
-    author='Cargometrics',
-    author_email='ajenkins@cargometrics.com',
+    name='pyflow-swf',
+    version='1.0',
+    author='Adam Jenkins',
+    author_email='adam@thejenkins.org',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2.7',
+    ],
     packages=['pyflow'],
-    url='http://www.cargometrics.com',
-    description='Python Workflow Library',
+    url='https://github.com/ajenkins-cargometrics/pyflow',
+    description='Python Workflow Library built on the AWS SWF service',
+    long_description=long_description,
     install_requires=[
         'boto3',
         'attrs'
     ],
     setup_requires=['pytest-runner'],
     tests_require=[
-        'pytest',
+        'pytest>=3.0',
     ]
 )
